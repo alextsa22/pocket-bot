cb87a98e274a843662c6833f9f004fbaf978dbc6
Alex TSA <alextsa22@gmail.com>
2021-03-14 23:41:42 +0600

    update .gitignore & init bot in main.go

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"github.com/alextsa22/pocket-bot/pkg/pocket"
+	"github.com/alextsa22/pocket-bot/pkg/telegram"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		log.Fatal("1", err)
+	}
+
+	bot.Debug = true
+
+	pocketClient, err := pocket.NewClient(os.Getenv("CONSUMER_KEY"))
+	if err != nil {
+		log.Fatal("2", err)
+	}
+
+	telegramBot := telegram.NewBot(bot, pocketClient, "http://localhost")
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal("3", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.7.0
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
