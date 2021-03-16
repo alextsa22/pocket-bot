c26c4d90f9a76a2f4b6cc35b222545f71943f650
Alex TSA <alextsa22@gmail.com>
2021-03-16 18:51:34 +0600

    implement adding a link to Pocket

--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -1,7 +1,10 @@
 package telegram
 
 import (
+	"context"
+	"github.com/alextsa22/pocket-bot/pkg/pocket"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"net/url"
 )
 
 const (
@@ -19,9 +22,34 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) error {
 	}
 }
 
-func (b *Bot) handleMessage(message *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
-	b.bot.Send(msg)
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "link saved successfully")
+
+	_, err := url.Parse(message.Text)
+	if err != nil {
+		msg.Text = "this is an invalid link"
+		_, err = b.bot.Send(msg)
+		return err
+	}
+
+	accessToken, err := b.getAccessToken(message.Chat.ID)
+	if err != nil {
+		msg.Text = "you are not authorized, use the /start command"
+		_, err = b.bot.Send(msg)
+		return err
+	}
+
+	if err := b.pocketClient.Add(context.Background(), pocket.AddInput{
+		AccessToken: accessToken,
+		URL:         message.Text,
+	}); err != nil {
+		msg.Text = "failed to save link, please try again"
+		_, err = b.bot.Send(msg)
+		return err
+	}
+
+	_, err = b.bot.Send(msg)
+	return err
 }
 
 func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
